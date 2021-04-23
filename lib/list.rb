93d54e064669330a7fe5a1e5966f830c0be4c80a
mmayes098 <mayes.matthewj@gmail.com>
2021-04-23 09:17:39 -0400

    Finished linked list

--- lib/list.rb
+++ lib/list.rb
@@ -29,4 +29,77 @@ class LinkedList
       @head = current
     end
   end
+
+  def size
+    count = 0
+    pointer = @head
+    return 0 if @head.nil?
+    return 1 if @head == @tail
+    until pointer.nil?
+      count += 1
+      pointer = pointer.next_node
+    end
+
+    count
+  end
+
+  def at(index)
+    count = 0
+    pointer = @head
+    return nil if index > size - 1
+
+    until pointer.nil?
+      return pointer.value if count == index
+      count += 1
+      pointer = pointer.next_node
+    end
+  end
+
+  def pop
+    return puts "Can't delete, no items in the list!" if @head.nil?
+
+    if size == 1
+      @head = nil
+      @tail = nil
+      return
+    end
+
+    pointer = @head
+    pointer = pointer.next_node until pointer.next_node == @tail
+    pointer.next_node = nil
+    @tail = pointer
+  end
+
+  def contains?(value)
+    pointer = @head
+    until pointer.nil?
+      return true if pointer.value == value
+      pointer = pointer.next_node
+    end
+
+    false
+  end
+
+  def find(value)
+    count = 0
+    pointer = @head
+    until pointer.nil?
+      return count if pointer.value == value
+      count += 1
+      pointer = pointer.next_node
+    end
+  end
+
+  def to_s
+    string = ''
+    count = 0
+    pointer = @head
+    until pointer.nil?
+      string << "( #{pointer.value} ) -> "
+      count += 1
+      pointer = pointer.next_node
+    end
+    string << 'nil'
+    puts string
+  end
 end
